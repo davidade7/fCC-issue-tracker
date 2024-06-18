ec6b26a2cbe733e24993fccc9810164c8f99dc52
David ADE <david.ade.1989@gmail.com>
2024-06-18 11:14:57 +0000

    adding mongoose

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,33 @@
 'use strict';
 
-module.exports = function (app) {
+const mongoose = require('mongoose')
 
-  app.route('/api/issues/:project')
+
+module.exports = function (app) {
+const api = "https://issue-tracker.freecodecamp.rocks/api/issues/apitest/"
+
+  app.route(api)
   
+
+
     .get(function (req, res){
       let project = req.params.project;
-      
+      console.log('get',req.body)
     })
     
     .post(function (req, res){
       let project = req.params.project;
-      
+      console.log('post',req.body)
     })
     
     .put(function (req, res){
       let project = req.params.project;
-      
+      console.log('put',req.body)
     })
     
     .delete(function (req, res){
       let project = req.params.project;
-      
+      console.log('delete',req.body)
     });
     
 };
