a8b0bd6e4e8a1c461f00d7f82a9b00a7a40b7416
David ADE <david.ade.1989@gmail.com>
2024-06-18 15:24:11 +0200

    adding connexion to database

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,33 @@
 'use strict';
-
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const mongoose = require('mongoose')
 
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connection to mongoose
+mongoose.connect(process.env['MONGO_URI']);
+
+const issueSchema = new mongoose.Schema({
+  issue_title: String,
+  issue_text: String,
+  created_on: Date,
+  updated_on: Date,
+  created_by: String,
+  assigned_to: String,
+  open: Boolean,
+  status_text: String
+})
+
+const Issue = mongoose.model('Issue', issueSchema);
 
 module.exports = function (app) {
-const api = "https://issue-tracker.freecodecamp.rocks/api/issues/apitest/"
+const api = "/api/issues/"
 
   app.route(api)
   
