1ae1d36f22dde9288fb9ff5c312d3da352b807dc
David ADE <david.ade.1989@gmail.com>
2024-06-20 13:17:26 +0200

    adding post route

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,14 @@
 'use strict';
-const express = require('express')
-const app = express()
-const cors = require('cors')
+
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // Connection to mongoose
-mongoose.connect(process.env['MONGO_URI']);
+mongoose.connect(process.env['MONGO_URI'])
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);;
 
+// Schema for the issues
 const issueSchema = new mongoose.Schema({
   issue_title: String,
   issue_text: String,
@@ -21,36 +17,65 @@ const issueSchema = new mongoose.Schema({
   created_by: String,
   assigned_to: String,
   open: Boolean,
-  status_text: String
+  status_text: String,
+  project_name: String
 })
 
+// Models
 const Issue = mongoose.model('Issue', issueSchema);
 
+// Routes
 module.exports = function (app) {
-const api = "/api/issues/"
 
-  app.route(api)
-  
+  app.route("/api/issues/:project")
 
+    // .get(function (req, res){
+    //   let project = req.params.project;
+    //   console.log('get',req.body)
+    // })
+    
+    .post((req, res) => {
+      try {
+        let projectName = req.params.project
+                
+        // Working with issue
+        const { issue_title, issue_text, created_by, assigned_to , status_text } = req.body
+        if (!issue_title || !issue_text || !created_by) {
+          res.json({error: "required field(s) missing"});
+          return;
+        }
 
-    .get(function (req, res){
-      let project = req.params.project;
-      console.log('get',req.body)
+        // Creating the issue
+        let issueToAdd = new Issue({
+          issue_title, 
+          issue_text, 
+          created_on: new Date(), 
+          updated_on: new Date(), 
+          created_by, 
+          assigned_to,
+          open: true, 
+          status_text,
+          project_name: projectName
+        })
+        issueToAdd.save()
+        res.json(issueToAdd)
+      }
+      catch (error) {
+        console.log(error)
+        res.json({
+          error: error.message
+        });
+      }
     })
     
-    .post(function (req, res){
-      let project = req.params.project;
-      console.log('post',req.body)
-    })
+    // .put(function (req, res){
+    //   let project = req.params.project;
+    //   console.log('put',req.body)
+    // })
     
-    .put(function (req, res){
-      let project = req.params.project;
-      console.log('put',req.body)
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      console.log('delete',req.body)
-    });
+    // .delete(function (req, res){
+    //   let project = req.params.project;
+    //   console.log('delete',req.body)
+    // });
     
 };
