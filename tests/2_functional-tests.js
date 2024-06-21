35c5adc7b7e3a21f72fce1684b333501ffffb70e
David ADE <david.ade.1989@gmail.com>
2024-06-21 11:16:11 +0200

    adding functionnal tests for GET request

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,14 +18,14 @@ suite('Functional Tests', function() {
 
     // Execute
     chai
-        .request(server)
-        .post("/api/issues/test")
-        .send({ issue })
-        .end(function(err, res) {
-            // Assert
-            assert.equal(res.status, 200);
-            done();
-        });
+      .request(server)
+      .post("/api/issues/test")
+      .send({ issue })
+      .end(function(err, res) {
+        // Assert
+        assert.equal(res.status, 200);
+        done();
+      });
   }),
   test('Test 1-2: Create an issue with only required fields: POST request', (done) => {
     // Setup
@@ -37,14 +37,14 @@ suite('Functional Tests', function() {
 
     // Execute
     chai
-        .request(server)
-        .post("/api/issues/test")
-        .send({ issue })
-        .end(function(err, res) {
-            // Assert
-            assert.equal(res.status, 200);
-            done();
-        });
+      .request(server)
+      .post("/api/issues/test")
+      .send({ issue })
+      .end(function(err, res) {
+        // Assert
+        assert.equal(res.status, 200);
+        done();
+      });
   }),
   test('Test 1-3: Create an issue with missing required fields: POST request', (done) => {
     // Setup
@@ -55,14 +55,83 @@ suite('Functional Tests', function() {
 
     // Execute
     chai
-        .request(server)
-        .post("/api/issues/test")
-        .send({ issue })
-        .end(function(err, res) {
-            // Assert
-            assert.equal(res.status, 200);
-            assert.equal(res.text,'{"error":"required field(s) missing"}');
-            done();
-        });
+      .request(server)
+      .post("/api/issues/test")
+      .send({ issue })
+      .end(function(err, res) {
+        // Assert
+        assert.equal(res.status, 200);
+        assert.equal(res.text,'{"error":"required field(s) missing"}');
+        done();
+      });
+  }),
+
+  test('Test 1-4: View issues on a project: GET request', (done) => {
+    // Execute
+    chai
+      .request(server)
+      .get("/api/issues/test")
+      .query({})
+      .end(function(err, res) {
+        // Assert
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.property(res.body[0], 'issue_title')
+        assert.property(res.body[0], 'issue_text')
+        assert.property(res.body[0], 'created_on')
+        assert.property(res.body[0], 'updated_on')
+        assert.property(res.body[0], 'created_by')
+        assert.property(res.body[0], 'assigned_to')
+        assert.property(res.body[0], 'open')
+        assert.property(res.body[0], 'status_text')
+        assert.property(res.body[0], '_id')
+        done();
+      });
+  })
+
+  test('Test 1-5: View issues on a project with one filter: GET request', (done) => {
+    // Execute
+    chai
+      .request(server)
+      .get("/api/issues/test")
+      .query({created_by: "Joe"})
+      .end(function(err, res) {
+        // Assert
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.property(res.body[0], 'issue_title')
+        assert.property(res.body[0], 'issue_text')
+        assert.property(res.body[0], 'created_on')
+        assert.property(res.body[0], 'updated_on')
+        assert.property(res.body[0], 'created_by')
+        assert.property(res.body[0], 'assigned_to')
+        assert.property(res.body[0], 'open')
+        assert.property(res.body[0], 'status_text')
+        assert.property(res.body[0], '_id')
+        done();
+      });
+  })
+
+  test('Test 1-6: View issues on a project with multiple filters: GET request', (done) => {
+    // Execute
+    chai
+      .request(server)
+      .get("/api/issues/test")
+      .query({created_by: "Joe", open: true})
+      .end(function(err, res) {
+        // Assert
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.property(res.body[0], 'issue_title')
+        assert.property(res.body[0], 'issue_text')
+        assert.property(res.body[0], 'created_on')
+        assert.property(res.body[0], 'updated_on')
+        assert.property(res.body[0], 'created_by')
+        assert.property(res.body[0], 'assigned_to')
+        assert.property(res.body[0], 'open')
+        assert.property(res.body[0], 'status_text')
+        assert.property(res.body[0], '_id')
+        done();
+      });
   })
 });
